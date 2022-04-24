8d22ac5d4cca442dc88536f405478343540e1c08
Mohamad J. Makki <malanbaki@transforat.com>
2022-04-24 13:21:10 +0300

    Postgres Roles

---
+++ src/main/java/app/springadmin/springadmin/Utility/Implement/UserDetailsServiceImpl.java
@@ -0,0 +1,28 @@
+package app.springadmin.springadmin.Utility.Implement;
+
+import app.springadmin.springadmin.DAO.UserDAO;
+import app.springadmin.springadmin.Entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class UserDetailsServiceImpl implements UserDetailsService {
+    UserDAO userDAO;
+
+    @Autowired
+    public UserDetailsServiceImpl(UserDAO userDAO) {
+        this.userDAO = userDAO;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userDAO.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
+        return UserDetailsImpl.build(user);
+    }
+}
